064079997401e8c097e34e15388cc297ad1d6622
salioundiaye1 <salioundiaye446@gmail.com>
2026-02-27 12:03:06 +0000

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="CV Digital - Saliou NDIAYE", layout="wide")
+
+
+with st.sidebar:
+    st.image("saliou.png", width=200)
+    st.markdown("## Saliou NDIAYE")
+
+    st.markdown("### 📞 Contact")
+    st.write("📱 76 947 70 31")
+    st.write("📧 salioundiaye446@gmail.com")
+    st.write("📍 Parcelles. Assainies, Dakar")
+
+    st.markdown("### 🎓 Formations")
+    st.write("**2025 - 2026 :** BTS en Géomatique")
+    st.write("**2021 - 2022 :** Licence 2 en Droit")
+    st.write("**2019 - 2020 :** Baccalauréat L2")
+
+
+st.title("Technicien Supérieur en Géomatique")
+
+st.subheader("À propos de moi")
+st.write("""
+Je suis à la recherche d’un environnement professionnel stimulant 
+favorisant l’apprentissage continu, la créativité et l’innovation. 
+Motivé et rigoureux, je souhaite développer mes compétences 
+tout en contribuant efficacement à la réussite des projets.
+""")
+
+st.subheader("Projets académiques")
+st.write("- **Programmation :** Conception et réalisation d’un CV digital avec Streamlit.")
+st.write("- **Projection cartographique :** Collecte et traitement de données autour de la Mosquée Massalikoul Djinane.")
+st.write("- **Base de données :** Géoréférencement de cartes et création de bases de données spatiales.")
+st.write("- **Techniques géographiques :** Élaboration de cartes thématiques et cartes de situation.")
+
+st.subheader("Expériences professionnelles")
+st.write("- **2025 :** Stage en topographie – Eiffage Sénégal.")
+st.write("- **2023 :** Agent de recensement (RGPH-5).")
+st.write("- **2023 :** Conducteur de travaux – Projet de construction de caniveaux (NEC).")
+
+st.subheader("Compétences")
+st.write("- Systèmes d’Information Géographique (SIG) : QGIS, ArcGIS.")
+st.write("- Conception de bases de données : Power AMC, PostgreSQL / PostGIS.")
+st.write("- Géoréférencement et numérisation de cartes.")
+st.write("- Collecte de données sur le terrain : Mobile Topographer, KoboToolbox.")
+st.write("- Conception de plans : AutoCAD.")
+st.write("- Maîtrise du Pack Office : Excel, Word, PowerPoint.")
+st.write("- Réalisation de levés topographiques.")
+
+st.subheader("Perspectives professionnelles")
+st.write("- Création d’un cabinet d’expertise en géomatique.")
+st.write("- Conception et administration de bases de données spatiales.")
+st.write("- Spécialisation en Systèmes d’Information Géographique (SIG).")
